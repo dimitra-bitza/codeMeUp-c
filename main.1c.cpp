b33297a3ddbbccd124daeed695761cd956f9708e
dimitra-bitza <48857087+dimitra-bitza@users.noreply.github.com>
2019-03-23 13:40:11 +0200

    my first commit

---
+++ main.1c.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	 float c=-1;
+	 float f=-1
+	 printf("dwse thermokrasia kelsiou: \n");
+	 scanf("%f ,&c");
+	 f=c*9/5+32;
+	 printf("%f vathmoi kelsiou einai %f vathmoi farenheit",c,f);
+	 return 0
+	 
+}
